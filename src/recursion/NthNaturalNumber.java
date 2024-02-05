ebbff04f24c0fffcb96bf5d5b4035f3d922c47ce
ankit gupta <ankitgu8430@gmail.com>
2024-02-05 20:48:54 +0530

    Natural Number

--- src/recursion/NthNaturalNumber.java
+++ src/recursion/NthNaturalNumber.java
@@ -1,5 +1,21 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class NthNaturalNumber {
-    
+    public static void printSum(int i,int n,int sum){
+        if(i==n){ //Base Case
+            sum+=i;
+            System.out.print(sum+" ");
+            return;
+        }
+        sum+=i;
+        printSum(i+1, n, sum);
+    }
+    public static void main(String [] args){
+        System.out.println("Enter the Nth Natural Number");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printSum(0,n,0);
+    }
 }
