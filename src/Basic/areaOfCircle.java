88ff77e2412ed1b9148d7e6f20e16d26981a6a50
ankit gupta <ankitgu8430@gmail.com>
2024-02-19 23:10:06 +0530

    area

--- src/Basic/areaOfCircle.java
+++ src/Basic/areaOfCircle.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class areaOfCircle {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Radius");
-        double r=sc.nextDouble();
+        System.out.println("Enter the Radius: ");
+        int r=sc.nextInt();
         double area=3.14*r*r;
-        System.out.println("Area of Circle is :"+area);
+        System.out.println("Area of Circle is ="+area);
     }
 }
