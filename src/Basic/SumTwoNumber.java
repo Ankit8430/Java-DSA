b45e232ebabf790e9dee643f2fadadeba2e201aa
ankit gupta <ankitgu8430@gmail.com>
2024-02-19 23:04:13 +0530

    SumofTwoNumber

---
+++ src/Basic/SumTwoNumber.java
@@ -0,0 +1,13 @@
+package Basic;
+import java.util.*;
+public class SumTwoNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number 1");
+        int num1=sc.nextInt();
+        System.out.println("Enter the Number 1");
+        int num2=sc.nextInt();
+        int sum=num1+num2;
+        System.out.println("Sum of "+num1+" and "+num2+" is ="+sum);
+    }
+}
